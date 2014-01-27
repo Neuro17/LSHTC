59fdcab0ffa75016c75afac753a7c39e3c72b939
Neuro17 <biagio.chirico.86@gmail.com>
2014-01-27 11:24:42 +0100

    add limit to data filtering

--- train_1000.py
+++ train_1000.py
@@ -3,10 +3,12 @@ import csv
 train = open('resource/train-remapped.csv', 'rb')
 train_1000 = open('resource/my_train.csv', 'wb')                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
 count = 0
+limit = 1 #limit numbers of data in training set
+
 for row in train:
     row = row.replace(', ', ',')
     train_1000.write(row)
     count += 1
-    if count == 1:
+    if count == limit:
         break
     
